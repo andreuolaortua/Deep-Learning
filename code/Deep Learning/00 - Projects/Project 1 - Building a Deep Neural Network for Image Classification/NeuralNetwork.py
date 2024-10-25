546dfaaabc8826746ba0eb4ecf677961997137d9
andreuolaortua <andreu.olaortua23@gmail.com>
2024-10-25 13:14:45 +0200

    First commit

---
+++ code/Deep Learning/00 - Projects/Project 1 - Building a Deep Neural Network for Image Classification/NeuralNetwork.py
@@ -0,0 +1,149 @@
+#Importing libraries
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from testCases import *
+from dnn_utils import sigmoid, sigmoid_backward, relu, relu_backward
+from public_tests import *
+
+import copy
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+np.random.seed(1)
+
+#Initializing parameters of the Weights and bias for all the layers of the NN
+def initialize_parameters_deep(layer_dims):
+    np.random.seed(3)  # Seed to ensure same random numbers from now on
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+        # Generates errors if the dimensions do not match to avoid wrong results
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+def linear_forward(A, W, b):
+    Z = np.dot(W, A) + b
+    cache = (A, W, b)
+
+    return Z, cache
+
+def linear_activation_forward(A_prev, W, b, activation):
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+def L_model_forward(X, parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2  # number of layers in the neural network
+
+    for l in range(1, L):
+        A_prev = A
+        W = parameters["W" + str(l)]
+        b = parameters["b" + str(l)]
+        A, cache = linear_activation_forward(A_prev, W, b, activation="relu")
+        caches.append(cache)
+
+    W = parameters["W" + str(L)]
+    b = parameters["b" + str(L)]
+    AL, cache = linear_activation_forward(A, W, b, activation="sigmoid")
+    caches.append(cache)
+
+    return AL, caches
+
+def compute_cost(AL, Y):
+
+    m = Y.shape[1]
+
+    cost = -1 / m * np.sum(np.dot(np.log(AL), Y.T) + np.dot((1 - Y), np.log(1 - AL).T))
+    cost = np.squeeze(cost)
+    return cost
+
+def linear_backward(dZ, cache):
+
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = 1/m*np.dot(dZ, A_prev.T)
+    db = 1/m*np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ)
+
+    return dA_prev, dW, db
+
+def linear_activation_backward(dA, cache, activation):
+
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    elif activation == "sigmoid":
+
+        dZ =sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches)  # the number of layers
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)  # after this line, Y is the same shape as AL
+
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))  # derivative of cost with respect to AL
+
+    current_cache = caches[L - 1]
+    dA_prev_temp, dW_temp, db_temp = linear_activation_backward(dAL, current_cache, activation="sigmoid")
+    grads["dA" + str(L - 1)] = dA_prev_temp
+    grads["dW" + str(L)] = dW_temp
+    grads["db" + str(L)] = db_temp
+
+    for l in reversed(range(L - 1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 1)], current_cache,
+                                                                    activation="relu")
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+    return grads
+
+def update_parameters(params, grads, learning_rate):
+
+    parameters = copy.deepcopy(params)
+    L = len(parameters) // 2 # number of layers in the neural network
+
+    for l in range(L):
+
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - grads["dW" + str(l+1)]*learning_rate
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - grads["db" + str(l+1)]*learning_rate
+
+    return parameters
+
+
+t_parameters, grads = update_parameters_test_case()
+t_parameters = update_parameters(t_parameters, grads, 0.1)
+
+print ("W1 = "+ str(t_parameters["W1"]))
+print ("b1 = "+ str(t_parameters["b1"]))
+print ("W2 = "+ str(t_parameters["W2"]))
+print ("b2 = "+ str(t_parameters["b2"]))
+
+update_parameters_test(update_parameters)
